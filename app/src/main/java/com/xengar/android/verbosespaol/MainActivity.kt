1f9bb313a02d50c53a1b0d7fb527686a0000b78a
Angel Newton <xanewton@gmail.com>
2018-02-28 17:27:24 -0500

    Change slide menu options

--- app/src/main/java/com/xengar/android/verbosespaol/MainActivity.kt
+++ app/src/main/java/com/xengar/android/verbosespaol/MainActivity.kt
@@ -72,22 +72,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_verbs -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_cards -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_favorites -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_settings -> {
 
             }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
+            R.id.nav_help -> {
 
             }
         }
